217867e035320845a6d999c9444964172f5c15eb
helloJosh <kimjoshua135@gmail.com>
2024-08-28 17:55:44 +0900

    백준 6064

--- beakjun/sw2/bruteforce/Beak6064.java
+++ beakjun/sw2/bruteforce/Beak6064.java
@@ -1,4 +1,36 @@
 package sw2.bruteforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Beak6064 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            boolean ok = false;
+
+            for (int i = x; i < n*m; i += m) {
+                if (i % n == y) {
+                    System.out.println(i + 1);
+                    ok = true;
+                    break;
+                }
+            }
+
+            if (!ok) {
+                System.out.println(-1);
+            }
+        }
+    }
 }
