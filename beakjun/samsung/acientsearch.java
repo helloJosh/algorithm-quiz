43b031451d3e209a317df477ebe9e8b42b847603
helloJosh <kimjoshua135@gmail.com>
2024-09-18 17:08:42 +0900

    고대문명유적 풀이

--- beakjun/samsung/acientsearch.java
+++ beakjun/samsung/acientsearch.java
@@ -7,97 +7,150 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class acientsearch {
-    public static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1}, dy= {-1, -1, 0, 1, 1, 1, 0, -1};
-    public static int k, m;
-    public static int[][] a = new int[6][6];
-    public static Queue<Integer> wall = new LinkedList<>();
-    public static int[][] count = new int[6][6];
-    public static boolean[][] check = new boolean[6][6];
+    static final int nLarge = 5;
+    static final int nSmall = 3;
 
-    public static int[][] turn(int[][] tempA, int y, int x) {
-        int[] temp = new int[10];
-        for (int i = 0; i < 8; i++) {
-            temp[i] = tempA[y + dy[i]][x + dx[i]];
-        }
-        for (int i = 0; i < 8; i++) {
-            temp[9 - i] = temp[7 - i];
-        }
-        temp[1] = temp[9];
-        temp[0] = temp[8];
+    static class Board {
+        int[][] a = new int[nLarge][nLarge];
 
-        for (int i = 0; i < 8; i++) {
-            tempA[y + dy[i]][x + dx[i]] = temp[i];
-        }
-
-        print(tempA);
-
-        return tempA;
-    }
-
-    public static Queue<int[]> doBfs(int[][] tempA, int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        Queue<int[]> history = new LinkedList<>();
-        int cnt = 1;
-        queue.add(new int[]{y, x});
-        history.add(new int[]{y, x});
-        check[y][x] = true;
-        count[y][x] = cnt;
-        while (!queue.isEmpty()) {
-            int[] p = queue.remove();
-            for (int i = 0; i < 8; i += 2) {
-                int ny = p[0] + dx[i];
-                int nx = p[1] + dy[i];
-                if (!check[ny][nx] && tempA[ny][nx] == tempA[y][x]) {
-                    queue.add(new int[]{ny, nx});
-                    history.add(new int[]{ny, nx});
-                    count[ny][nx] = ++cnt;
-                    check[ny][nx] = true;
+        public Board() {
+            for (int i = 0; i < nLarge; i++) {
+                for (int j = 0; j <nLarge; j++) {
+                    a[i][j] = 0;
                 }
             }
         }
 
-        if (cnt >= 3) {
-            return history;
-        } else {
-            return new LinkedList<>();
+        public boolean inRange(int y, int x) {
+            return 0 <= y && y < nLarge && 0 <= x && x < nLarge;
         }
-    }
 
-    public static void get(List<int[]> list) {
-
-    }
-    public static void print(int[][] a){
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j] + " ");
+        public Board rotate(int sy, int sx, int cnt) {
+            Board result = new Board();
+            for (int i = 0; i < nLarge; i++) {
+                for (int j = 0; j < nLarge; j++) {
+                    result.a[i][j] = this.a[i][j];
+                }
             }
-            System.out.println();
+
+            for (int i = 0; i < cnt; i++) {
+                int tmp = result.a[sy + 0][sx + 2];
+                result.a[sy + 0][sx + 2] = result.a[sy + 0][sx + 0];
+                result.a[sy + 0][sx + 0] = result.a[sy + 2][sx + 0];
+                result.a[sy + 2][sx + 0] = result.a[sy + 2][sx + 2];
+                result.a[sy + 2][sx + 2] = tmp;
+                tmp = result.a[sy + 1][sx + 2];
+                result.a[sy + 1][sx + 2] = result.a[sy + 0][sx + 1];
+                result.a[sy + 0][sx + 1] = result.a[sy + 1][sx + 0];
+                result.a[sy + 1][sx + 0] = result.a[sy + 2][sx + 1];
+                result.a[sy + 2][sx + 1] = tmp;
+            }
+            return result;
+        }
+
+        public int calScore() {
+            int score = 0;
+            boolean[][] visit = new boolean[nLarge][nLarge];
+            int[] dy = { 0, 1, 0, -1}, dx = {1, 0, -1, 0};
+
+            for (int i = 0; i < nLarge; i++) {
+                for (int j = 0; j < nLarge; j++) {
+                    if (!visit[i][j]) {
+                        Queue<int[]> q = new LinkedList<>();
+                        Queue<int[]> trace = new LinkedList<>();
+
+                        q.add(new int[]{i, j});
+                        trace.add(new int[]{i, j});
+                        visit[i][j] = true;
+                        while (!q.isEmpty()) {
+                            int[] cur = q.remove();
+                            for (int k = 0 ; k < 4; k++) {
+                                int ny = cur[0] + dy[k], nx = cur[1] +dx[k];
+                                if (inRange(ny, nx) && a[ny][nx] == a[cur[0]][cur[1]] && !visit[ny][nx]) {
+                                    q.add(new int[]{ny, nx});
+                                    trace.add(new int[]{ny, nx});
+                                    visit[ny][nx] = true;
+                                }
+                            }
+                        }
+
+                        if (trace.size() >= 3) {
+                            score += trace.size();
+                            while (!trace.isEmpty()) {
+                                int[] t = trace.remove();
+                                a[t[0]][t[1]] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return score;
+        }
+
+        public void fill(Queue<Integer> queue) {
+            for (int j = 0; j < nLarge; j++) {
+                for (int i = nLarge - 1; i >= 0; i--) {
+                    if (a[i][j] == 0 && !queue.isEmpty()) {
+                        a[i][j] = queue.remove();
+                    }
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        k = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new LinkedList<>();
+        Board board = new Board();
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = 0; i < nLarge; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < 6; j++) {
-                a[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < nLarge; j++) {
+                board.a[j][i] = Integer.parseInt(st.nextToken());
             }
         }
-
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < m; i++) {
-            wall.add(Integer.parseInt(st.nextToken()));
+        for (int i= 0; i < m; i++) {
+            q.add(Integer.parseInt(st.nextToken()));
         }
 
-        int[][] tempA = turn(a, 3 ,3);
+        while (k-- > 0) {
+            int maxScore = 0;
+            Board maxScoreBoard = null;
 
-        print(count);
+            for (int cnt = 1; cnt <= 3; cnt++) {
+                for (int sx = 0; sx <= nLarge - nSmall; sx++) {
+                    for (int sy = 0; sy <= nLarge - nSmall; sy++) {
+                        Board rotated = board.rotate(sy, sx, cnt);
+                        int score = rotated.calScore();
+                        if (maxScore < score) {
+                            maxScore = score;
+                            maxScoreBoard = rotated;
+                        }
+                    }
+                }
+            }
 
+            if (maxScoreBoard == null) {
+                break;
+            }
+
+            board = maxScoreBoard;
+            while (true) {
+                board.fill(q);
+                int newScore = board.calScore();
+                if (newScore == 0) {
+                    break;
+                }
+                maxScore += newScore;
+            }
+
+            System.out.println(maxScore + " ");
+        }
     }
 }
