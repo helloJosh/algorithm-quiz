a1c4702668c2242f9cb90456529c50261b6783c2
helloJosh <kimjoshua135@gmail.com>
2024-10-06 20:25:07 +0900

    feat: test updated

--- beakjun/Main.java
+++ beakjun/Main.java
@@ -1,41 +1,9 @@
 import java.util.*;
 
 public class Main {
-    static final int inf = -100000000;
-    static List<Integer> t;
-    static List<Integer> p;
-    static int n;
-    static int ans = 0;
-
-    static void go(int day, int sum) {
-        if (day == n + 1) {
-            if (ans < sum) ans = sum;
-            return;
-        }
-        if (day > n + 1) {
-            return;
-        }
-        go(day + 1, sum);
-        go(day + t.get(day), sum + p.get(day));
-    }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        t = new ArrayList<>(n + 1);
-        p = new ArrayList<>(n + 1);
-
-        // ArrayList의 초기 값을 채우기 위해 0을 넣어줍니다.
-        for (int i = 0; i <= n; i++) {
-            t.add(0);
-            p.add(0);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            t.set(i, sc.nextInt());
-            p.set(i, sc.nextInt());
-        }
-        go(1, 0);
-        System.out.println(ans);
+    public static void main(String[] args) {
+        int i = 0;
+        int t = i % 2;
+        System.out.println(t);
     }
 }
